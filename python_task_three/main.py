c6ed6807116232a2ce9c729c1491e8861e65e388
anton <l2u7ck@gmail.com>
2024-04-30 19:30:14 +0300

    ChangeFileThree

--- python_task_three/main.py
+++ python_task_three/main.py
@@ -8,13 +8,29 @@ class Student:
         self.grades = {}
 
     def rate_lecturer(self, lecturer, course, marks):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if (isinstance(lecturer, Lecturer)
+                and course in lecturer.courses_attached
+                and course in self.courses_in_progress):
             if course in lecturer.marks:
                 lecturer.marks[course] += [marks]
             else:
                 lecturer.marks[course] = [marks]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        sumall = 0
+        count = 0
+        for mark in self.grades.values():
+            sumall += sum(mark)
+            count += len(mark)
+        srmarks = sumall / count
+        return (f"Имя: {self.name}"
+                f"\nФамилия: {self.surname}"
+                f"\nСредняя оценка за домашнее задание: {srmarks}"
+                f"\nКурсы в процессе изучения: {', '.join(self.courses_in_progress)}"
+                f"\nЗавершенные курсы: {', '.join(self.finished_courses)}")
+
 
 
 class Mentor:
@@ -29,10 +45,23 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.marks = {}
 
+    def __str__(self):
+        sumall = 0
+        count = 0
+        for mark in self.marks.values():
+            sumall += sum(mark)
+            count += len(mark)
+        srmarks = sumall / count
+        return (f"Имя: {self.name}"
+                f"\nФамилия: {self.surname}"
+                f"\nСредняя оценка за лекции: {srmarks}")
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if (isinstance(student, Student)
+                and course in self.courses_attached
+                and course in student.courses_in_progress):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -40,16 +69,19 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python', 'Git']
+best_student.finished_courses += ['Введение в программирование']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Python', 'Git']
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Git', 6)
 
 cool_lecturer = Lecturer('Fred', 'Black')
 cool_lecturer.courses_attached += ['Python']
@@ -58,7 +90,9 @@ cool_lecturer2 = Lecturer('Fred', 'Black')
 cool_lecturer2.courses_attached += ['Git']
 
 best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+best_student.rate_lecturer(cool_lecturer, 'Python', 8)
 best_student.rate_lecturer(cool_lecturer2, 'Git', 9)
 
 print(cool_lecturer.marks)
 print(cool_lecturer2.marks)
+print(best_student)
